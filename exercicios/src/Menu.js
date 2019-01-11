886c98b1eb6e51788ead3788884d41597eedebba
Guilherme Catini <guilherme.catini@iv2.com.br>
2019-01-11 14:20:19 -0200

    first commit

---
+++ exercicios/src/Menu.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { createDrawerNavigator } from 'react-navigation'
+
+import Simples from './components/Simples'
+import ParImpar from './components/ParImpar'
+import { Inverter, MegaSena } from './components/Multi'
+import Contador from './components/Contador'
+import Plataformas from './components/Plataformas'
+import ValidarProps from './components/ValidarProps'
+import Evento from './components/Evento'
+import { Avo } from './components/ComunicacaoDireta'
+import { TextoSincronizado } from './components/ComunicacaoIndireta'
+import ListaFlex from './components/ListaFlex'
+import Flex from './components/Flex'
+
+export default createDrawerNavigator({
+    Flex: {
+        screen: Flex
+    },
+    ListaFlex: {
+        screen: ListaFlex,
+        navigationOptions: { title: 'Lista (Flex Box)' }
+    },
+    TextoSincronizado: {
+        screen: TextoSincronizado,
+        navigationOptions: { title: 'Texto Sincronizado' }
+    },
+    Avo: {
+        screen: () => <Avo nome="João" sobrenome="Silva" />
+    },
+    Evento: {
+        screen: Evento
+    },
+    ValidarProps: {
+        screen: () => <ValidarProps ano={1} />
+    },
+    Plataformas: {
+        screen: Plataformas
+    },
+    Contador: {
+        screen: () => <Contador numeroInicial={777} />
+    },
+    MegaSena: {
+        screen: () => <MegaSena numeros={8} />,
+        navigationOptions: { title: 'Mega Sena' }
+    },
+    Inverter: {
+        screen: () => <Inverter texto='DRACULA' />
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />
+    },
+    Simples: {
+        screen: () => <Simples texto="Flexível" />
+    }
+},
+    {
+        drawerWidth: 300
+    })
---
+++ mines/src/components/Mine.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default props => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.coreMine} />
+            <View style={styles.line} />
+            <View style={[styles.line, { transform: [{ rotate: '45deg' }] }]} />
+            <View style={[styles.line, { transform: [{ rotate: '90deg' }] }]} />
+            <View style={[styles.line, { transform: [{ rotate: '135deg' }] }]} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    coreMine: {
+        width: 14,
+        height: 14,
+        borderRadius: 10,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    line: {
+        position: 'absolute',
+        height: 3,
+        width: 20,
+        borderRadius: 3,
+        backgroundColor: 'black'
+    }
+})
