b74694dd311839b2a75ae72cd2fcaf41788f8aa0
Guilherme Catini <guilherme.catini@iv2.com.br>
2019-01-22 16:45:49 -0200

    ok

--- tasks-backend/index.js
+++ tasks-backend/index.js
@@ -2,7 +2,12 @@ const app = require('express')()
 const db = require('./config/db')
 const consign = require('consign')
 
-consign().then('./config/middlewares.js').into(app)
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.db = db
 
