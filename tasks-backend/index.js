fd46ccc4266fa2e7243b043e26abf4bcf87eba10
Guilherme Catini <guilherme.catini@iv2.com.br>
2019-01-22 14:42:50 -0200

    Ok

---
+++ tasks-backend/index.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign().then('./config/middlewares.js').into(app)
+
+app.db = db
+
+app.listen(3000, () => {
+    console.log('backend executando...')
+})
---
+++ tasks/src/commonStyles.js
@@ -0,0 +1,10 @@
+export default {
+    fontFamily: 'Lato',
+    colors: {
+        default: '#1631BE',
+        today: '#B13B44',
+        secondary: '#FFF',
+        mainText: '#222',
+        subText: '#555'
+    }
+}
